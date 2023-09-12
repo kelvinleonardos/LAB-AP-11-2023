b4ab6a50ae0481c34bb97be43a203f4c99bebb5d
Kelvin Leonardo Sianipar <113580597+kelvinleonardos@users.noreply.github.com>
2023-09-12 17:16:27 +0800

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-import os
-
-w = ["H071231021", "H071231047", "H071231091", "H071231044", "H071231025", "H071231073"]
-
-for i in w:
-    os.mkdir(i)
-    with open(i + "/readme.md", "w") as f:
-        f.write(" ")
