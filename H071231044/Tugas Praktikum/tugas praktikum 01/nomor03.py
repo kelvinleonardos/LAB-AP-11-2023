abbb8f193d40514fb17243f25ecd25276e0800e2
Mohammad180305 <moh.abd.razaq@gmail.com>
2023-09-21 13:04:40 +0700

    Tugas Praktikum

---
+++ H071231044/Tugas Praktikum/tugas praktikum 01/nomor03.py
@@ -0,0 +1,16 @@
+
+print("No.3")
+print("Solusi persamaan kuadrat")
+a = float(input("a = "))
+while a == 0:
+    print("a tidak boleh 0")
+    a = float(input("a = "))
+
+b = float(input("b = "))
+c = float(input("c = "))
+
+x1 = (-b+(b**2-4*a*c)**0.5)/(2*a)
+print(f"x1 = {x1:.2f}")
+
+x2 = (-b-(b**2-4*a*c)**0.5)/(2*a)
+print(f"x2 = {x2:.2f}")
