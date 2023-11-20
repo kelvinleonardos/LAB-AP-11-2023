a9af48a32421c5f3242f68d9afb00d6a6ba49cb4
Mohammad180305 <moh.abd.razaq@gmail.com>
2023-11-20 13:44:01 +0800

    tugas 10

--- H071231044/Tugas Praktikum/tugas praktikum 10/nomor 02.py
+++ H071231044/Tugas Praktikum/tugas praktikum 10/nomor 02.py
@@ -1,34 +1,27 @@
-class mobil :
-    def __init__(self,merk,kecepatan,jarak) :
+from abc import ABC, abstractmethod
+
+class mobil(ABC) :
+    def _init_(self,merk,kecepatan,jarak):
         self._merk = merk
         self._kecepatan = kecepatan
         self._jarak = jarak
+    def info(self):
+        print(f"mobil {self._merk} dengan kecepatan {self._kecepatan} km/jam dapat bergerak sejauh {self._jarak} km")
+class bmw(mobil):
+    def _init_(self,merk,kecepatan,jarak):
+        super(). _init_(merk,kecepatan,jarak)
+    def bensin(self):
+        print("jenis bahan bakar : Premium")
+class pajero(mobil):
+    def _init_(self,merk,kecepatan,jarak):
+        super(). _init_(merk,kecepatan,jarak)
+    def bensin(self):
+        print("jenis bahan bakar : Solar")
+        
+mobil1 = bmw("bmw",100,500)
+mobil1.info()
+mobil1.bensin()
 
-    def get_merk(self):
-        return self._merk
-
-    def get_kecepatan(self):
-        return self._kecepatan
-
-    def get_jarak(self):
-        return self._kecepatan
-
-
-class BMW(mobil):
-        def __init__(self,merk,kecepatan,jarak) :
-            super().__init__(merk,kecepatan,jarak)
-        def klakson(self):
-            print("Bunyi Klakson : Rawr")
-
-class Toyota(mobil):
-        def __init__(self,merk,kecepatan,jarak) :
-            super().__init__(merk,kecepatan,jarak)
-        def klakson(self):
-            print("Bunyi Klakson : Beep")
-
-mbl1 = BMW("BMW",100,500)
-print(f"Mobil {mbl1.get_merk} dengan kecepatan {mbl1._kecepatan} km/jam dapat bergerak sejauh {mbl1._jarak} km tanpa henti")
-mbl1.klakson()
-mbl2 = Toyota("Toyota",60,300)
-print(f"Mobil {mbl2._merk} dengan kecepatan {mbl2._kecepatan} km/jam dapat bergerak sejauh {mbl2._jarak} km tanpa henti")
-mbl2.klakson()
+mobil2 = pajero("pajero",120,600)
+mobil2.info()
+mobil2.bensin()
